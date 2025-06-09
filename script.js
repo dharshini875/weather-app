f1500a98654180a62f8cda27f2d34bd96f41d97d
dharshini875 <137678264+dharshini875@users.noreply.github.com>
2025-06-09 19:36:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getWeather() {
   const city = document.getElementById('cityInput').value;
-  const apiKey = 'abfdf94d43682324940ffc4180ecf35d';
+  const apiKey = 'YOUR_API_KEY';
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   const res = await fetch(url);
